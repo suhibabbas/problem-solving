a568b2ccb4b9136060bca0783e364a9a16e3453e
ghl1590 <sohaib_salah96@yahoo.com>
2022-01-24 11:52:15 +0200

    finished the task

--- Challenge06/Challenge06.js
+++ Challenge06/Challenge06.js
@@ -31,6 +31,14 @@ const findMax = (arr)=>{
 
 const sumNums = (arr)=>{
     let sum;
+    sum =0
+    for( let i =0; i <arr.length; i ++){
+        if(typeof arr[i] == 'number'){
+           sum = sum +arr[i];
+        }
+        
+        
+    }
     return sum;
 }
 
